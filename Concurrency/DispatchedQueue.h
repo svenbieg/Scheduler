b2241f6de2270887ef3124f7db1f8d06f00db73e
Sven Bieg <svenbieg@web.de>
2024-12-27 09:32:36 +0100

    Removed MainTask

--- Concurrency/DispatchedQueue.h
+++ Concurrency/DispatchedQueue.h
@@ -29,6 +29,22 @@ class DispatchedQueue
 public:
 	// Common
 	static VOID Append(DispatchedHandler* Handler);
+	static inline VOID Append(VOID (*Procedure)())
+		{
+		Append(new DispatchedProcedure(Procedure));
+		}
+	template <class _owner_t> static inline VOID Append(_owner_t* Owner, VOID (_owner_t::*Procedure)())
+		{
+		Append(new DispatchedMemberProcedure<_owner_t>(Owner, Procedure));
+		}
+	template <class _owner_t, class _lambda_t> static inline VOID Append(_owner_t* Owner, _lambda_t&& Lambda)
+		{
+		Append(new DispatchedLambda<_owner_t, _lambda_t>(Owner, std::forward<_lambda_t>(Lambda)));
+		}
+	template <class _owner_t, class _lambda_t> static inline VOID Append(Handle<_owner_t> Owner, _lambda_t&& Lambda)
+		{
+		Append(new DispatchedLambda<_owner_t, _lambda_t>(Owner, std::forward<_lambda_t>(Lambda)));
+		}
 	static VOID Enter();
 	static VOID Exit();
 
